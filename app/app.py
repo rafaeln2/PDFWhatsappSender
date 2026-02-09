ac688fff6319184cb6bedf49dee71458fbdb1925
rafaeln2 <rafaeln_nascimento@hotmail.com>
2026-02-08 23:33:27 -0300

    somewhat working whatsapp message sender

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,15 +7,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
 
-CONTACT = os.getenv("WHATSAPP_CONTACT") # Nome do contato na agenda
-PDF_FOLDER = os.getenv("PDF_FOLDER") # pasta onde vai ter os arquivos PDF
-CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH") # driver do navegador chrome
-
 load_dotenv()
 
+# Load environment variables with safe string defaults so types are `str` (not `None`).
+CONTACT = os.getenv("WHATSAPP_CONTACT", "")  # Nome do contato na agenda
+FILE_FOLDER = Path(os.getenv("FILE_FOLDER", default=""))  # pasta onde vai ter os arquivos PDF
+CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH", "")  # driver do navegador chrome
+
 def send_pdfs():
-    service = Service(CHROMEDRIVER_PATH)
-    driver = webdriver.Chrome(service=service)
+    # service = Service(CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome()
 
     driver.get("https://web.whatsapp.com")
     input("📲 Escaneie o QR Code e aperte ENTER...")
@@ -28,20 +30,24 @@ def send_pdfs():
     time.sleep(2)
 
     # Enviar PDFs
-    for arquivo in os.listdir(PDF_FOLDER):
+    for arquivo in os.listdir(FILE_FOLDER):
         if arquivo.lower().endswith(".png"):
-            caminho = os.path.join(PDF_FOLDER, arquivo)
+            caminho = os.path.join(FILE_FOLDER, arquivo)
             print(f"Enviando: {arquivo}")
 
-            clip_button = driver.find_element(By.XPATH, "//div[@title='Anexar']")
-            clip_button.click()
+            annex_button = driver.find_element(By.XPATH, "//button[@aria-label='Anexar']")            
+            annex_button.click()
+            time.sleep(1)
+            
+            document_div = driver.find_element(By.XPATH, "//div[@aria-label='Documento']")
+            document_div.click()
             time.sleep(1)
 
             file_input = driver.find_element(By.XPATH, "//input[@accept='*']")
             file_input.send_keys(caminho)
             time.sleep(2)
 
-            send_button = driver.find_element(By.XPATH, "//span[@data-icon='send']")
+            send_button = driver.find_element(By.XPATH, "//div[@aria-label='Enviar']")
             send_button.click()
             time.sleep(1)
 
